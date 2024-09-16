544685d5d0e5281837b1f3497c8fcf731411b530
Milind Khariwale <milindk@fermion.in>
2024-09-16 15:18:05 +0530

    performance improve

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -8,7 +8,7 @@ function Aboutme() {
       </div>
       <div class="container" >
         <div class="container_image">
-          <img src="/images/About-me.png" />
+          <img src="/images/About-me.png" alt='About-me'/>
         </div><div class="about-container">
           <p>I am a dedicated and passionate React Developer with over 2 years of hands-on experience in building dynamic and responsive web applications. My journey in web development has equipped me with a solid understanding of front-end technologies and a deep expertise in ReactJS, allowing me to craft seamless user experiences.
           <h3>Professional Summary</h3>
